074b4128cb15e2fa76af6d6233d032e5ed147bf5
Christian Nachtrieb <chris.nachtrieb@gmail.com>
2022-02-19 14:42:36 -0500

    Initial Commit

---
+++ Picnic Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picnic Weather
+//
+//  Created by Christian Nachtrieb on 2/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Picnic Weather/Picnic_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picnic_WeatherApp.swift
+//  Picnic Weather
+//
+//  Created by Christian Nachtrieb on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Picnic_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
